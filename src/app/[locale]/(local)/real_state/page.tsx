6233b011349badf492f28fb88e30cc9342d359b5
Yuri Takahashi <ty_tsuruya@icloud.com>
2023-12-29 17:00:28 +0900

    小林不動産セクション追加

--- src/app/[locale]/(local)/real_state/page.tsx
+++ src/app/[locale]/(local)/real_state/page.tsx
@@ -12,7 +12,15 @@ const ProductPage = () => {
           {t("RealState.Headline")}
         </h1>
         <Image src="/images/realstate.png" alt="小林不動産" />
-        <div className="mt-8 text-lg whitespace-pre-wrap">{t("RealState.Description")}</div>
+        <div className="mt-8 mb-20 text-lg whitespace-pre-wrap">{t("RealState.Description")}</div>
+        <div className="text-center">
+          <div className="text-xl mb-4">小林不動産</div>
+          <div className="mb-2">代表 小林 幸次郎</div>
+          <div className="mb-2">京都市上京区千本通下立売上る稲葉町470番地</div>
+          <div className="mb-2">TEL 075-811-5985</div>
+          <div className="mb-2">京都府知事(11)第6344号</div>
+          <div className="mb-2">提携会社 大手不動産会社数社と提携</div>
+        </div>
       </div>
     </main>
   );
